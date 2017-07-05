7ff70b9a027e0626584f030a97ae9d9868bf0b8e
Xuan Dieu <xuandieubmt189@gmail.com>
2017-07-05 16:42:01 +0700

    CONPLETE

--- Cau2/ViewController.swift
+++ Cau2/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var txtUrl1: UITextField!
     @IBOutlet weak var txtUrl2: UITextField!
+   
     @IBOutlet weak var imageView1: UIImageView!
-    
     @IBOutlet weak var imageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
